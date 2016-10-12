85c073f8fbc9c4f6904cdbf3f4bc8a1b32ffd7a9
juniorpires <juniorpiresupe@gmail.com>
2016-10-11 21:49:50 -0300

    criado cenario

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ $(function(){
 
     const JOGADOR = 1;
     const PAREDE = 2;
-    const CAIXA = 3; 
+    const CAIXA = 3;
+    const MARCA = 4;  
 
-    var jogador = new Bloco("#jogador",JOGADOR,20,20);
-    var parede1 = new Bloco("#parede1",PAREDE,60,60);
-    var parede2 = new Bloco("#parede2",PAREDE,60,120);
+    var jogador = new Bloco("#jogador",JOGADOR,160,240);
+    var parede1 = new Bloco("#parede1",PAREDE,0,0);
+    var parede2 = new Bloco("#parede2",PAREDE,0,400);
+    var parede3 = new Bloco("#parede3",PAREDE,0,80);
+    var parede4 = new Bloco("#parede4",PAREDE,480,80);
+    var parede5 = new Bloco("#parede5",PAREDE,160,160);
 
-    var caixa1 = new Bloco("#caixa1",CAIXA,60,40);
-    var caixa2 = new Bloco("#caixa2",CAIXA,120,20);
-    var caixa3 = new Bloco("#caixa3",CAIXA,80,0);
+    var caixa1 = new Bloco("#caixa1",CAIXA,240,160);
+    var caixa2 = new Bloco("#caixa2",CAIXA,240,240);
+    var caixa3 = new Bloco("#caixa3",CAIXA,320,240);
+
+    var marca1 = new Bloco("#marca1",MARCA,320,160);
+    var marca2 = new Bloco("#marca2",MARCA,240,240);
+    var marca3 = new Bloco("#marca3",MARCA,320,240);
 
 
-    var blocos = [parede1,parede2,caixa1,caixa2,caixa3];
-    var paredes = [parede1,parede2,caixa1,caixa2,caixa3];
+    var blocos = [parede1,parede2,parede3,parede4,parede5,caixa1,caixa2,caixa3];
+    var paredes = [parede1,parede2,parede3,parede4,parede5,caixa1,caixa2,caixa3];
 
 
     var main = $("#main"); 
@@ -49,14 +57,14 @@ $(function(){
             elemColidido = jogador.colisao(LEFT,blocos);
            if(elemColidido==null){ 
 
-              jogador.mover(0,-20);
+              jogador.mover(0,-80);
 
            }else if(elemColidido.getTipo()==CAIXA){
               
               if(elemColidido.colisao(LEFT,paredes)==null){
                   if(leftMain(elemColidido)){
-                    elemColidido.mover(0,-20);
-                    jogador.mover(0,-20);
+                    elemColidido.mover(0,-80);
+                    jogador.mover(0,-80);
                   }
                }
            }
@@ -65,12 +73,12 @@ $(function(){
          if(e.keyCode=='38' && topMain(jogador)){
            elemColidido = jogador.colisao(UP,blocos);  
            if(elemColidido==null){ 
-             jogador.mover(-20,0);
+             jogador.mover(-80,0);
            }else if(elemColidido.getTipo()==CAIXA){
              if(elemColidido.colisao(UP,paredes)==null){
                  if(topMain(elemColidido)){
-                   elemColidido.mover(-20,0);
-                   jogador.mover(-20,0);
+                   elemColidido.mover(-80,0);
+                   jogador.mover(-80,0);
                  }
                }
            }
@@ -79,12 +87,12 @@ $(function(){
          if(e.keyCode=='39' && rightMain(jogador)){
              elemColidido = jogador.colisao(RIGHT,blocos);
             if(elemColidido==null){ 
-              jogador.mover(0,20);
+              jogador.mover(0,80);
            }else if(elemColidido.getTipo()==CAIXA){
                if(elemColidido.colisao(RIGHT,paredes)==null){
                    if(rightMain(elemColidido)){
-                      elemColidido.mover(0,20);
-                      jogador.mover(0,20);
+                      elemColidido.mover(0,80);
+                      jogador.mover(0,80);
                    }
                }
            }
@@ -93,16 +101,36 @@ $(function(){
          if(e.keyCode=='40' && bottomMain(jogador)){
              elemColidido = jogador.colisao(DOWN,blocos);
              if(elemColidido==null){    
-               jogador.mover(20,0);
+               jogador.mover(80,0);
             }else if(elemColidido.getTipo()==CAIXA){
                if(elemColidido.colisao(DOWN,paredes)==null){
                    if(bottomMain(elemColidido)){
-                      elemColidido.mover(20,0);
-                      jogador.mover(20,0);
+                      elemColidido.mover(80,0);
+                      jogador.mover(80,0);
                    }
                }
            }
         }
+
+
+       if(( (caixa1.top==marca1.top && caixa1.left==marca1.left) 
+       || (caixa2.top==marca1.top && caixa2.left==marca1.left) 
+       || (caixa3.top==marca1.top && caixa3.left==marca1.left)  ) 
+       
+       &&( (caixa1.top==marca2.top && caixa1.left==marca2.left) 
+       || (caixa2.top==marca2.top && caixa2.left==marca2.left) 
+       || (caixa3.top==marca2.top && caixa3.left==marca2.left)  ) 
+       
+       &&( (caixa1.top==marca3.top && caixa1.left==marca3.left) 
+       || (caixa2.top==marca3.top && caixa2.left==marca3.left) 
+       || (caixa3.top==marca3.top && caixa3.left==marca3.left)  ))  {
+           
+           
+           $("p").css("visibility","visible");
+       }
+
+
+
     });
 
 
@@ -141,16 +169,16 @@ $(function(){
            outros.forEach(function(outro) {
                switch (direcao){
                    case UP:
-                      colidiu = ((outro.top + outro.div.height()) == this.top) && (this.left>=outro.left && this.left<outro.left+outro.div.width());
+                      colidiu = ((outro.top + outro.div.height()) == this.top) && ((this.left+this.div.width())>outro.left && this.left<outro.left+outro.div.width());
                       break;
                    case DOWN:
-                      colidiu = outro.top == (this.top + this.div.height()) && (this.left>=outro.left && this.left<outro.left+outro.div.width());
+                      colidiu = outro.top == (this.top + this.div.height()) && (this.left+this.div.width()>outro.left && this.left<outro.left+outro.div.width());
                       break;
                    case LEFT:
-                      colidiu = ((outro.left + outro.div.width()) == this.left) && (this.top>=outro.top && this.top<outro.top+outro.div.height());   
+                      colidiu = ((outro.left + outro.div.width()) == this.left) && ((this.top+this.div.height())>outro.top && this.top<outro.top+outro.div.height());   
                       break;
                    case RIGHT:
-                      colidiu = outro.left == (this.left + this.div.width()) && (this.top>=outro.top && this.top<outro.top+outro.div.height());
+                      colidiu = outro.left == (this.left + this.div.width()) && (this.top+this.div.height()>outro.top && this.top<outro.top+outro.div.height());
                       break;
                    default:
                        colidiu=false;   
